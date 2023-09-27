fb197acbb19a1676636418052e7d953293c99ddf
Sebastian Parfitt <Seb.Parfitt@me.com>
2023-09-27 12:22:12 +0100

    Added serializable DateTime (WIP)

--- Sebstensions.cs
+++ Sebstensions.cs
@@ -2416,6 +2416,45 @@ public static class Directions
 }
 
 [Serializable]
+public class SDateTime : IComparable<SDateTime>
+{
+	[SerializeField]
+	private long m_ticks;
+
+	private bool initialized;
+	private DateTime m_dateTime;
+	public DateTime DateTime
+	{
+		get
+		{
+			if (!initialized)
+			{
+				m_dateTime = new DateTime(m_ticks);
+				initialized = true;
+			}
+
+			return m_dateTime;
+		}
+	}
+
+	public SDateTime(DateTime dateTime)
+	{
+		m_ticks = dateTime.Ticks;
+		m_dateTime = dateTime;
+		initialized = true;
+	}
+
+	public int CompareTo(SDateTime other)
+	{
+		if (other == null)
+		{
+			return 1;
+		}
+		return m_ticks.CompareTo(other.m_ticks);
+	}
+}
+
+[Serializable]
 public class SDictionary<TKey, TValue> : Dictionary<TKey, TValue>, ISerializationCallbackReceiver
 {
 	[SerializeField, HideInInspector]
